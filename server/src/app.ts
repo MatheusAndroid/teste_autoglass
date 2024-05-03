b9156c796d63cb6247744dd6e067f54c7a920a64
Matheus Luiz <matluiz78@gmail.com>
2024-05-03 01:23:56 -0300

    cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express';
 import { createProduct, getAllProducts, getProductById, updateProduct } from './presentation/controllers/ProductController';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins: string[] = ['http://localhost:4200'];
+
+const corsOptions: cors.CorsOptions = {
+    origin: allowedOrigins,
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.get('/products/:id', getProductById);
 app.get('/products', getAllProducts);
