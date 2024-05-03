9d722492cb436e30d39a0e98ed264979226e6e62
Matheus Luiz <matluiz78@gmail.com>
2024-05-03 01:23:12 -0300

    roteamento e página inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { NewProductComponent } from './new-product/new-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductsComponent, },
+  { path: 'product-item/:id', component: ProductItemComponent },
+  { path: 'new-product', component: NewProductComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
